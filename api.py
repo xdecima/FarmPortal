9379f72aa28c339d21c5fbf15de6243ed68b2d86
xdecima <pasquale.palmisano@gmail.com>
2025-09-13 12:34:18 +0100

    basic API to calculate area

--- api.py
+++ api.py
@@ -1,11 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 
 def index():
-    return jsonify({"message": "Hello World !"})
+    data = request.get_json()
+    print(data)
+    a = area(data["length"], data["depth"])
+
+    return jsonify({"risultato": a})
+
+
+def area(lenght, depth):
+    return lenght * depth
 
 
 def create_app():
     app = Flask(__name__)
-    app.add_url_rule("/", view_func=index)
+    _ = CORS(app)
+
+    app.add_url_rule("/calculate", view_func=index, methods=["POST"])
     return app
